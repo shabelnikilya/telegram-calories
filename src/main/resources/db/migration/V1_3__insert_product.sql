e68ad5f927b30433c32605e722fcdc2862227db9
shabelnikilya <shabelnik_ilya@mail.ru>
2022-05-15 22:41:16 +0300

    Add product sql script.

---
+++ src/main/resources/db/migration/V1_3__insert_product.sql
@@ -0,0 +1,38 @@
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Абрикосовый сок', 0.9, 0.2, 9.2, 39);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Ананасовый сок', 0.2, 0.2, 11.4, 48);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Апельсиновый сок', 0.9, 0.1, 8.4, 36);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Виноградный сок', 0.3, 0, 14.5, 56);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Вишневый сок', 0.5, 0, 10.6, 49);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Гранатовый сок', 0.2, 0, 14, 58);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Какао на молоке', 24, 17, 33.1, 377);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Квас хлебный', 0.2, 0, 5.0, 26);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Кола', 0, 0, 10.0, 40);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Кофе с молоком', 0.8, 1.0, 11.0, 56);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Лимонад', 0, 0, 6.1, 24);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Лимонный сок', 1.0, 0.1, 3.2, 18);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Морковный сок', 1.0, 0.1, 6.5, 31);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Персиковый сок', 0.8, 0.1, 9.1, 37);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Пиво безалкогольное', 0, 0, 4.1, 22);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Зеленый чай', 0, 0, 0, 0);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Черный чай без сахара', 0, 0, 0, 0);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Энергетический напиток', 0, 0, 11.4, 47);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Яблочный сок', 0.5, 0.4, 9.7, 42);

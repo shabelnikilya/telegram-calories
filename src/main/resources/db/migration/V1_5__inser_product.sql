637fa0990765f5653fd2030d3077b9fe71beea46
Admin <shaidurov.ilha@yandex.ru>
2022-05-24 22:23:05 +0300

    Add sql script product.

---
+++ src/main/resources/db/migration/V1_5__inser_product.sql
@@ -0,0 +1,10 @@
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Икра кеты зернистая', 31.6, 13.7, 0, 250);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Икра лещевая пробойная', 24.6, 4.9, 0, 144);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Икра минтая пробойная', 28.3, 1.8, 0, 127);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Икра осетровая зернистая', 28.3, 9.6, 0, 201);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Икра осетровая пробойная', 36.3, 10.1, 0, 235);
---
+++ src/main/resources/db/migration/V1_8__insert_product.sql
@@ -0,0 +1,20 @@
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Жир куриный', 0, 99.7, 0, 896);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Жир свиной топленый', 0, 99.5, 0, 882);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Маргарин сливочный', 0.5, 82.3, 0,	746);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Маргарин столовый молочный', 0.5, 82, 0.9, 744);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Майонез 67%', 3.3, 67, 2.4, 624);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Масло льняное', 0, 99.8, 0, 898);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Масло оливковое', 0, 99.8, 0, 898);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Масло подсолнечное', 0, 99.9, 0, 899);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Масло сливочное 82,5%', 0.5, 82.5,	1, 747);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Масло топленое', 0.4, 98.1, 0.5, 885);

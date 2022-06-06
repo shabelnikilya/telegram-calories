bcd9eb6b6e80de7adb8116d7ca925cc5a14fc9bf
shabelnikilya <shabelnik_ilya@mail.ru>
2022-06-06 21:02:55 +0300

    Add sql script

---
+++ src/main/resources/db/migration/V1_9__insert_product.sql
@@ -0,0 +1,54 @@
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Йогурт 1.5%', 4.3, 1.5, 8.4, 65);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Йогурт 3.2%', 5.0, 3.2, 8.9, 87);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Кефир 0%', 2.8, 0, 3.8, 29);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Кефир 1%', 2.8, 1, 4.0, 37);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Кефир 2.5%', 3.0, 2.5, 4.0, 51);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Кефир 3.2%', 3.2, 3.2, 4.1, 57);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Молоко козье сырое', 3.1, 4.2, 4.7, 71);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Молоко коровье сырое', 3.2, 3.6, 4.7, 63);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Молоко обезжиренное', 2.1, 0.1, 4.5, 30);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Молоко сухое цельное', 25.2, 25, 39.6, 477);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Молоко сгущенное', 7.3, 7.7, 9.7, 139);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Простакваша 3.2%', 2.9, 3.2, 4.0, 57);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Ряженка 2.5%', 2.9, 2.5, 4.1, 53);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Ряженка 4.0%', 2.9, 4.0, 4.1, 68);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Сливки 10%', 2.8, 10.0, 4.1, 121);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Сливки 20%', 2.8, 20.0, 3.9, 209);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Сметана 10%', 3.0, 10, 2.9, 118);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Сметана 15%', 3.0, 15, 2.9, 163);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Сметана 20%', 3.0, 20, 2.9, 208);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Сырки из творожной массы', 7.3, 23, 27.6, 344);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Сыр голландский', 26.4, 26.5, 0, 352);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Сыр пошехонский', 26.4, 26.3, 0, 348);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Сыр российский', 24.1, 29.8, 0.4, 366);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Сыр сулугуни', 20.0, 24.2, 0, 293);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Творог жирный', 14, 18, 1.9, 236);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Творог не жирный', 18.2, 0.6, 1.8, 89);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Творог полужирный', 16.5, 9.0, 1.9, 156);

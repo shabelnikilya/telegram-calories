fe69b69c723da399b43c0231ca342d65daea3b99
shabelnikilya <shabelnik_ilya@mail.ru>
2022-06-18 18:44:27 +0300

    Add sql script

---
+++ src/main/resources/db/migration/V1_10__insert_product.sql
@@ -0,0 +1,50 @@
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Баранина', 16.2, 15.3, 0, 201);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Бараньи почки', 13.4, 2.6, 0, 78);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Баранья печень', 18.9, 2.8, 0, 102);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Баранье сердце', 13.6, 2.7, 0, 85);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Говядина', 18.7, 12.6, 0, 191);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Говяжьи мозги', 9.3, 9.6, 0, 126);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Говяжья печень', 17.6, 3.2, 0, 100);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Говяжьи почки', 12.4, 1.9, 0, 67);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Говяжье вымя', 12.1, 13.8, 0, 176);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Говяжье сердце', 15.2, 3.1, 0, 89);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Говяжий язык', 13.4, 12.1, 0, 160);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Конина', 20.3, 7.1, 0, 149);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Кролик', 20.6, 12.8, 0, 197);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Свинина нежирная', 16.3, 27.9, 0, 318);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Свинина жирная', 11.6, 49.1, 0, 484);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Свиные почки', 13.2, 3.2, 0, 84);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Свиная печень', 18.6, 3.5, 0, 105);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Свиное сердце', 15.2, 3.1, 0, 87);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Свиной язык', 14.4, 16.5, 0, 203);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Телятина', 19.9, 1.1, 0, 91);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Гуси', 16.4, 33.1, 0, 359);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Индейка', 21.1, 12.3, 0.6, 192);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Куры', 20.4, 8.6, 0.8, 161);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Цыплята', 18.5, 7.9, 0.5, 159);
+INSERT INTO products(name, proteins, fats, carbohydrates, kcal)
+VALUES ('Утки', 16.4, 61.3, 0, 348);
